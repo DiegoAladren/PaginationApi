f5f19855976e022dd8ccc0fd8d5c6bd118489ead
DiegoAladren <146423034+DiegoAladren@users.noreply.github.com>
2025-06-20 17:48:25 +0200

    Initial commit

---
+++ src/test/java/com/example/springboot/paginationapi/SpringbootPaginationFilteringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.paginationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPaginationFilteringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
